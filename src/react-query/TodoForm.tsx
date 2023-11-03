3dfade1047469c94db9707c2081ce8ef3b216cc7
Bogdan Bucur <bogdanrbucur@gmail.com>
2023-11-03 22:20:23 +0200

    data mutation

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,18 +1,57 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
-  const ref = useRef<HTMLInputElement>(null);
+	const queryClient = useQueryClient();
 
-  return (
-    <form className="row mb-3">
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
-  );
+	const addTodo = useMutation<Todo, Error, Todo>({
+		mutationFn: (todo: Todo) => axios.post<Todo>("https://jsonplaceholder.typicode.com/todos", todo).then((res) => res.data),
+		onSuccess: (savedTodo, newTodo) => {
+			console.log(savedTodo);
+
+			// APPROACH 1: Invalidating the cache
+			// queryClient.invalidateQueries({
+			// 	queryKey: ["todos"],
+			// });
+
+			// APPROACH 2: Updating the data in the cache
+			queryClient.setQueryData<Todo[]>(["todos"], (todos) => [savedTodo, ...(todos || [])]);
+
+			if (ref.current) ref.current.value = "";
+		},
+	});
+	const ref = useRef<HTMLInputElement>(null);
+
+	return (
+		<>
+			{addTodo.error && <div className="alert alert-danger">{addTodo.error.message}</div>}
+			<form
+				className="row mb-3"
+				onSubmit={(event) => {
+					event.preventDefault();
+					if (ref.current && ref.current.value) {
+						addTodo.mutate({
+							id: 0,
+							title: ref.current?.value,
+							completed: false,
+							userId: 1,
+						});
+					}
+				}}
+			>
+				<div className="col">
+					<input ref={ref} type="text" className="form-control" />
+				</div>
+				<div className="col">
+					<button disabled={addTodo.isLoading} className="btn btn-primary">
+						{addTodo.isLoading ? "Adding..." : "Add"}
+					</button>
+				</div>
+			</form>
+		</>
+	);
 };
 
 export default TodoForm;
